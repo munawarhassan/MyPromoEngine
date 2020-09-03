188e36b3e020db5a779691ea4554ce77dc686a40
Munawar <munawar.hassan@in.ibm.com>
2020-09-04 05:05:49 +0530

    pom updated & first rule added

--- src/main/java/com/custompromoengine/rule/PromoFirstRule.java
+++ src/main/java/com/custompromoengine/rule/PromoFirstRule.java
@@ -10,7 +10,21 @@ public class PromoFirstRule implements BaseRule {
 	@Override
 	public void evaluateCondition(ProductOrdered productOrdered, List<ProductOrdered> productlistWithOffer,
 			List<ProductOrdered> productlistWithOutOffer) {
-		// TODO Auto-generated method stub
+	    String skuId =    productOrdered.getSkuId(); 
+	    String name = productOrdered.getName();
+	    String description = productOrdered.getDescription();
+	    int quantity = productOrdered.getQuantity();
+	    Double price = productOrdered.getPrice();
+		boolean offervailable = productOrdered.isOffervailable();		
+		
+		int offer_not_eligible =productOrdered.getQuantity() % 3;
+		int offer_eligible = productOrdered.getQuantity() - offer_not_eligible;	
+		ProductOrdered tempProductOffered = new ProductOrdered( skuId, name, description, quantity, price, offervailable);		
+		tempProductOffered.setQuantity(offer_eligible);
+		productlistWithOffer.add(tempProductOffered);
+		ProductOrdered tempProductNotOffered1 = new ProductOrdered( skuId, name, description, quantity, price, offervailable);		
+		tempProductNotOffered1.setQuantity(offer_not_eligible);
+		productlistWithOutOffer.add(tempProductNotOffered1);	
 		
 	}
 
@@ -23,8 +37,8 @@ public class PromoFirstRule implements BaseRule {
 
 	@Override
 	public Double calculateOfferedPrice(ProductOrdered productOrdered) {
-		// TODO Auto-generated method stub
-		return null;
+		Double price = (productOrdered.getQuantity()/3) * 130.00;
+		return price;
 	}
 
 	
